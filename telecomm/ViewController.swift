66839222a8e92392c70c731d14be2684e7cc4b52
Julie Chen <chenjulie1202@gmail.com>
2021-07-01 20:55:53 -0400

    Initial Commit

---
+++ telecomm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  telecomm
+//
+//  Created by Julie Chen on 7/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
